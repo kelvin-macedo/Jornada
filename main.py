63e083c2ae2971fe59e0d6f58933a9a9d2e6c592
Kelvin Macedo <macedo_kelvin@hotmail.com>
2025-05-12 12:13:31 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 33
+nome = "Kelvin"
+
+# Print
+print (idade)
+
+# Soma
+nova_idade = idade + 1
+print (nova_idade)
+
+# Lista
+lista= [1,2,3,4,5]
+
+print(lista)
+
+# Métodos
+lista_invertida = lista.reverse()
+print(lista)
